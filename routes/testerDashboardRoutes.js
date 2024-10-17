c794b04ecab2c3d51a26da4cb8c67bd2d70e89dc
rahithkr <rahithkr69@gmail.com>
2024-10-17 21:28:37 +0530

    testerprofileedit

--- routes/testerDashboardRoutes.js
+++ routes/testerDashboardRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { upload } from '../middlewares/fileUpload.js';
 
-import {listAllTasks,dashboardCount,testCaseCreation,listTestCases,updateBugReport,listSubmitterReport} from '../controllers/testerController/testerController.js'
+import {listAllTasks,dashboardCount,testCaseCreation,listTestCases,updateBugReport,listSubmitterReport, testerProfile, editTesterProfile} from '../controllers/testerController/testerController.js'
 
 
 const router = express.Router();
@@ -12,6 +12,6 @@ router.post('/testCaseCreation/:id',testCaseCreation)
 router.get('/listTestCases/:id',listTestCases)
 router.post('/bugreport', upload.array('files', 5),updateBugReport)
 router.get('/submited_report/:testerId',listSubmitterReport)
-
-
+router.get('/testerprofile/:Id',testerProfile)
+router.put('/updatetesterprofile/:userId',editTesterProfile)
 export default router; 
