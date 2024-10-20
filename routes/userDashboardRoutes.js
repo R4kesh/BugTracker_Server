f7f5882616d064d73499682f994bd46bb05901c8
Rakes S <rakeshsrks2580@gmail.com>
2024-10-20 14:23:09 +0530

    cookie

--- routes/userDashboardRoutes.js
+++ routes/userDashboardRoutes.js
@@ -2,18 +2,18 @@ import express from 'express';
 
 import {displayCount,getNewTasks,userAcceptTask
 ,listTasks,changeStatus,reAssignedList,updateStatus} from '../controllers/userController/userController.js'
-
+import { validateToken } from '../middlewares/validateToken.js';
 
 
 const router = express.Router();
 
-router.get('/count',displayCount)
-router.get('/newtasks/user',getNewTasks)
-router.put('/tasks/verifyByUser',userAcceptTask)
-router.get('/listApprovedTasks/:userId',listTasks)
-router.put('/changeTasksStatus/:taskId',changeStatus)
-router.get('/reassignedList',reAssignedList)
-router.put('/updateTaskStatus',updateStatus)
+router.get('/count',validateToken,displayCount)
+router.get('/newtasks/user',validateToken,getNewTasks)
+router.put('/tasks/verifyByUser',validateToken,userAcceptTask)
+router.get('/listApprovedTasks/:userId',validateToken,listTasks)
+router.put('/changeTasksStatus/:taskId',validateToken,changeStatus)
+router.get('/reassignedList',validateToken,reAssignedList)
+router.put('/updateTaskStatus',validateToken,updateStatus)
 
 
 export default router; 
